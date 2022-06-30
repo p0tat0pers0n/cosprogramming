2915a0eb01e0c6dbfc7b1b04cdc067cc9e69d7e6
p0tat0pers0n <ryankayser@mmc.school.nz>
2022-07-01 09:25:39 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace _201COS_Game
+{
+    partial class FrmGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlGame = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // PnlGame
+            // 
+            this.PnlGame.Location = new System.Drawing.Point(12, 63);
+            this.PnlGame.Name = "PnlGame";
+            this.PnlGame.Size = new System.Drawing.Size(760, 552);
+            this.PnlGame.TabIndex = 0;
+            // 
+            // FrmGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.PnlGame);
+            this.DoubleBuffered = true;
+            this.Name = "FrmGame";
+            this.Text = "Angry Nerds";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlGame;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _201COS_Game
+{
+    public partial class FrmGame : Form
+    {
+        public FrmGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
