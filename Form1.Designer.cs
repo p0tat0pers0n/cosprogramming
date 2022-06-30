1c868d31c4d9d11fc8f5318b750747931b5638b7
p0tat0pers0n <ryankayser@mmc.school.nz>
2022-07-01 09:39:23 +1200

    Basic Game Layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,32 +30,117 @@ namespace _201COS_Game
         private void InitializeComponent()
         {
             this.PnlGame = new System.Windows.Forms.Panel();
+            this.TextName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TextScore = new System.Windows.Forms.Label();
+            this.LblScore = new System.Windows.Forms.Label();
+            this.TextLives = new System.Windows.Forms.Label();
+            this.LblLives = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // PnlGame
             // 
+            this.PnlGame.BackgroundImage = global::_201COS_Game.Properties.Resources.Background;
+            this.PnlGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.PnlGame.Location = new System.Drawing.Point(12, 63);
             this.PnlGame.Name = "PnlGame";
-            this.PnlGame.Size = new System.Drawing.Size(760, 552);
+            this.PnlGame.Size = new System.Drawing.Size(660, 490);
             this.PnlGame.TabIndex = 0;
+            // 
+            // TextName
+            // 
+            this.TextName.AutoSize = true;
+            this.TextName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextName.ForeColor = System.Drawing.Color.White;
+            this.TextName.Location = new System.Drawing.Point(12, 9);
+            this.TextName.Name = "TextName";
+            this.TextName.Size = new System.Drawing.Size(62, 22);
+            this.TextName.TabIndex = 1;
+            this.TextName.Text = "Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Please input your name";
+            // 
+            // TextScore
+            // 
+            this.TextScore.AutoSize = true;
+            this.TextScore.BackColor = System.Drawing.Color.Transparent;
+            this.TextScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextScore.ForeColor = System.Drawing.Color.White;
+            this.TextScore.Location = new System.Drawing.Point(410, 9);
+            this.TextScore.Name = "TextScore";
+            this.TextScore.Size = new System.Drawing.Size(62, 22);
+            this.TextScore.TabIndex = 3;
+            this.TextScore.Text = "Score:";
+            // 
+            // LblScore
+            // 
+            this.LblScore.AutoSize = true;
+            this.LblScore.BackColor = System.Drawing.SystemColors.Control;
+            this.LblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblScore.Location = new System.Drawing.Point(478, 11);
+            this.LblScore.Name = "LblScore";
+            this.LblScore.Size = new System.Drawing.Size(20, 22);
+            this.LblScore.TabIndex = 4;
+            this.LblScore.Text = "0";
+            // 
+            // TextLives
+            // 
+            this.TextLives.AutoSize = true;
+            this.TextLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextLives.ForeColor = System.Drawing.Color.White;
+            this.TextLives.Location = new System.Drawing.Point(542, 11);
+            this.TextLives.Name = "TextLives";
+            this.TextLives.Size = new System.Drawing.Size(57, 22);
+            this.TextLives.TabIndex = 5;
+            this.TextLives.Text = "Lives:";
+            // 
+            // LblLives
+            // 
+            this.LblLives.AutoSize = true;
+            this.LblLives.BackColor = System.Drawing.SystemColors.Control;
+            this.LblLives.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLives.Location = new System.Drawing.Point(605, 11);
+            this.LblLives.Name = "LblLives";
+            this.LblLives.Size = new System.Drawing.Size(20, 22);
+            this.LblLives.TabIndex = 6;
+            this.LblLives.Text = "5";
             // 
             // FrmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.ClientSize = new System.Drawing.Size(684, 564);
+            this.Controls.Add(this.LblLives);
+            this.Controls.Add(this.TextLives);
+            this.Controls.Add(this.LblScore);
+            this.Controls.Add(this.TextScore);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TextName);
             this.Controls.Add(this.PnlGame);
             this.DoubleBuffered = true;
             this.Name = "FrmGame";
             this.Text = "Angry Nerds";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel PnlGame;
+        private System.Windows.Forms.Label TextName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label TextScore;
+        private System.Windows.Forms.Label LblScore;
+        private System.Windows.Forms.Label TextLives;
+        private System.Windows.Forms.Label LblLives;
     }
 }
 
